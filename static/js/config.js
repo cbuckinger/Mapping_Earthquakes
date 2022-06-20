0269a71f05a5944f258a20a5be9f98c693083456
cbuckinger <101474477+cbuckinger@users.noreply.github.com>
2022-06-20 11:47:42 -0700

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY2J1Y2tpbmdlciIsImEiOiJjbDRtejZydDQwMDN3M2RvYTk0cm9sYmc2In0.v5w-X-8WTz6ssstGBDpCTg";
+//pk.eyJ1IjoiY2J1Y2tpbmdlciIsImEiOiJjbDRtejZydDQwMDN3M2RvYTk0cm9sYmc2In0.v5w-X-8WTz6ssstGBDpCTg
